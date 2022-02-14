53b8276fcb4da53cb1d4b7e88cca517c2dc044b1
unknown <chilamakooru.gangaiiit@gmail.com>
2022-02-14 18:36:24 +0530

    1_Employee

--- Program.cs
+++ Program.cs
@@ -4,20 +4,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace EmployeeWageComputation
+namespace EmployeeDailyWage
 {
-    internal class EmployeeWageComputation
+    internal class EmployeeDailyWage
     {
-        public static void Main()
-        {
-            int IS_FULL_TIME = 1;
-            Random random = new Random();
-            int empcheck = random.Next(0,2);
-            if (empcheck == IS_FULL_TIME)
-                Console.WriteLine("Employee is present");
-            else
-                Console.WriteLine("Employee is absent");
-        }
-    }
-}
 
+
+        public static void Main(string[] args)
+        {
+            const int IS_FULL_TIME = 1;
+            const int EMP_RATE_PER_HOUR = 20;
+            const int IS_ABSENT = 0;
+            int WORKING_HOURS = 8;
+
+
+            //variables
+            int empWage = 0;
+
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if (empCheck == IS_FULL_TIME)
+                WORKING_HOURS = 8;
+            
+            // empHrs = 0;
+            empWage = WORKING_HOURS * EMP_RATE_PER_HOUR;
+            Console.WriteLine($"Employee Daily Earned Money {empWage}");
+            Console.Read();
+        }
+
+    }
+
+}
