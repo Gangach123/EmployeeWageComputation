7f06f1d5414f8209131fd177ac1c419e540e79ae
unknown <chilamakooru.gangaiiit@gmail.com>
2022-02-14 22:35:58 +0530

    EmployeeMonthSalary

--- Program.cs
+++ Program.cs
@@ -6,43 +6,45 @@ using System.Threading.Tasks;
 
 namespace EmployeeWageComputation
 {
-    class EmployeeWageComputation
+    internal class EmployeeWageComputation
     {
-        public static void Main(string[] args)
-
+        static void Main1(string[] args)
         {
             const int IS_FULL_TIME = 1;
             const int IS_PART_TIME = 2;
             const int IS_ABSENT = 0;
-            Console.WriteLine($"Employee Wage Calculator");
-            int workingHr = 1;
-            int perHrSalary = 20;
+            const int WAGEPERHOUR = 20;
+            const int TOTALWORKINGDAYS = 20;
 
-            Random random = new Random();
-            int present = (random.Next(0,3));
+            int workingHours = 8;
+            int totalWorkingHrs = 0;
 
-            switch (present)
+            for (int i = 0; i < TOTALWORKINGDAYS; i++)
             {
-
-                case IS_PART_TIME:
-                    Console.WriteLine("Employee is absent");
-                    workingHr = 8;
-                    break;
-
-                case IS_FULL_TIME:
-                    Console.WriteLine("Employee is present");
-                    workingHr = 8;
-                    break;
-                default:
-                    Console.WriteLine("Employee is absent");
-                    break;
+                Random random = new Random();
+                int employeeCheck = random.Next(0, 3);
+                switch (employeeCheck)
+                {
+                    
+                    case IS_PART_TIME:
+                        Console.WriteLine("Employee is part-time working");
+                        workingHours = 8;
+                        break;
+                    case IS_FULL_TIME:
+                        Console.WriteLine("Employee is present");
+                        workingHours = 8;
+                        break;
+                    default:
+                        Console.WriteLine("Employee is absent");
+                        workingHours = 0;
+                        break;
+                }
+                totalWorkingHrs = totalWorkingHrs + workingHours;
             }
-
-            int salary = perHrSalary * workingHr;
-            Console.WriteLine($"Salary {salary}");
+            int empWage = totalWorkingHrs * WAGEPERHOUR;
+            Console.WriteLine($"Employee earning money {empWage}");
             Console.ReadLine();
 
         }
     }
 }
-
