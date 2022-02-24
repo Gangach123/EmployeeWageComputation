15050276a1f5270222e82777edae93df73bd0669
unknown <chilamakooru.gangaiiit@gmail.com>
2022-02-24 21:34:30 +0530

    Wafe_For_Multiple_Companies

---
+++ EmployeeAttendence.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpWage_OOPS
+{
+    class EmployeeWageBuilder
+    {
+        int count = 0;
+
+        EmployeeWage[] employeeWages;
+
+        public EmployeeWageBuilder(int size)
+        {
+            this.employeeWages = new EmployeeWage[size];
+        }
+        public void AddEmployeeInfo(string companyName, int maxDay, int maxHrs, int wagePerHr)
+        {
+            this.employeeWages[count] = new EmployeeWage(companyName, maxDay, maxHrs, wagePerHr);
+            count++;
+        }
+
+        public void CalWage()
+        {
+            foreach (var empWageObj in employeeWages)
+            {
+                int totalWage = GetEmpWage(empWageObj);
+                empWageObj.SetTotalEmpWage(totalWage);
+                Console.WriteLine(empWageObj.ToString());
+            }
+        }
+        public int GetEmpWage(EmployeeWage employeeWages)
+        {
+            int workingHrs = 0;
+            int day = 0;
+            int totalWorkingHrs = 0;
+            int empTotalWage = 0;
+
+
+
+            while (day < employeeWages.maxDay && totalWorkingHrs <= employeeWages.maxHrs)
+            {
+
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+
+                //Constants
+                const int ABSENT = 0;
+                const int FULLTIME = 1;
+
+                switch (empCheck)
+                {
+                    case ABSENT:
+                        workingHrs = 0;
+                        break;
+
+                    case FULLTIME:
+                        workingHrs = 8;
+                        break;
+
+                    default:
+                        workingHrs = 4;
+                        break;
+                }
+
+                totalWorkingHrs = totalWorkingHrs + workingHrs;
+
+                int empWage = employeeWages.wagePerHr * workingHrs;
+                empTotalWage = empTotalWage + empWage;
+                day++;
+            }
+            return empTotalWage;
+        }
+
+
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,76 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace EmployeeWageComputation
+namespace EmpWage_OOPS
 {
-    internal class EmployeeWageComputation
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            const int IS_FULL_TIME = 1;
-            const int IS_PART_TIME = 2;
-            const int IS_ABSENT = 0;
-            const int WAGE_PER_HOUR = 20;
-            const int TOTAL_WORKING_DAYS = 20;
-            const int MAX_HRS_WORKED = 100;
-            int workingHours = 8;
-            int totalWorkingHrs = 0;
-            int workingDays = 0;
+            Console.WriteLine("Welcome to Employee Wage Calculation problem solving with OOPS concept");
 
-                Random random = new Random();
-                int employeeCheck = random.Next(0, 3);
-                switch (employeeCheck)
-                {
-                    
-                    case IS_PART_TIME:
-                        Console.WriteLine("Employee is working for parttime");
-                        workingHours = 8;
-                        break;
-
-                    case IS_FULL_TIME:
-                        Console.WriteLine("Employee is working for fulltime");
-                        workingHours = 8;
-                        break;
-
-                    default:
-                        Console.WriteLine("Employee is absent");
-                        workingHours = 0;
-                        break;
-                }
-          
-            }
-            static void Main1(string[] args)
-            {
-            const int WAGEPERHOUR = 20;
-            const int TOTALWORKINGDAYS = 20;
-            const int MAXHRSWORKED = 100;
-
-
-            int workingHrs = 0;
-            int totalWorkingHrs = 0;
-            int workingDays = 0;
-            int totalWage = 0;
-            int[] dailyWageArray = new int[20];
-            int[] totalWageArray = new int[20];
-
-            while (workingDays < TOTALWORKINGDAYS && totalWorkingHrs < MAXHRSWORKED)
-            {
-                workingHrs = Main2();
-                dailyWageArray[workingDays] = WAGEPERHOUR * workingHrs;
-                totalWage = totalWage + WAGEPERHOUR * workingHrs;
-                totalWageArray[workingDays] = totalWage;
-                totalWorkingHrs = totalWorkingHrs + workingHrs;
-                workingDays++;
-
-            }
-            int empWage = totalWorkingHrs * WAGEPERHOUR;
-            Console.WriteLine($"Employee earned Money {empWage}");
-            Console.WriteLine(String.Join(" ", dailyWageArray));
-            Console.WriteLine(String.Join(" ", totalWageArray));
-            Console.ReadLine();
-
+            EmployeeWageBuilder employeeWageBuilder = new EmployeeWageBuilder(4);
+            employeeWageBuilder.AddEmployeeInfo("Tata", 20, 100, 20);
+            employeeWageBuilder.AddEmployeeInfo("Dmart", 25, 125, 25);
+            employeeWageBuilder.AddEmployeeInfo("Reliance", 30, 140, 35);
+            employeeWageBuilder.AddEmployeeInfo("BigBazar", 23, 110, 22);
+            employeeWageBuilder.CalWage();
         }
-
     }
-              
-}
